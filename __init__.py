d473b27d8f3950f0e3b055526fb4c5253524a3b3
WenyNg <125436484+WenyNg@users.noreply.github.com>
2023-03-12 21:09:24 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from anfis.membership import membershipfunction
+from anfis.membership import mfDerivs
+from anfis.membership import Anfis
---
+++ membership/__init__.py
@@ -0,0 +1,3 @@
+import anfis.membership.membershipfunction
+import anfis.membership.mfDerivs
+import anfis.membership.Anfis
